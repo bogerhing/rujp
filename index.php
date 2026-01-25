ddcb6d7c744aeff7cb4cc759ff95790b6f9bae9e
bogerhing <bogerhinglevd162@gmail.com>
2026-01-25 02:42:07 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,16 @@
 <!DOCTYPE html>
 <html lang="ru2">
 <head>
+	<head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-4FJ70WDYYZ"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-4FJ70WDYYZ');
+</script>
 <!-- <script>
         document.addEventListener("DOMContentLoaded", function () {
 
